7405b8e52c4cf31a55559bd394d8cc085a5b5d2d
Askari Iqbal <askari.i@justsnap.de>
2024-02-10 22:00:09 +0500

    Adding Cache for quran lists

--- src/components/ListOfQuranLists.js
+++ src/components/ListOfQuranLists.js
@@ -33,23 +33,65 @@ const QuranList = ({obj}) => {
 
 const ListOfQuranLists = () => {
 
+  const LOCAL_STORAGE_QL_KEY = "ql"
   const dispatch = useDispatch()
   const quranList = useSelector(state => state.page.quranList)
   const [showRetryButton , setShowRetryButton] = useState(false)
 
-  const updateQuranList = () => {
-    setShowRetryButton(false)
-
-    const API = "https://quran-list-backend.vercel.app"
-    axios.get(`${API}/quranList/getAllLists`)
+  const updateDataFromServer = () => {
+    axios.get(`${process.env.REACT_APP_API}/quranList/getAllLists`)
       .then(response => {
         const quranList = response.data
         dispatch(setQuranList(quranList))
+        setDataFromToLocalStorage(JSON.stringify(quranList))
       })
       .catch(e => {
         setShowRetryButton(true)
         console.log(e)
       })
+  }
+
+  const isDateBeforeToday = (date) => {
+    return new Date(new Date(date).toDateString()) <= new Date(new Date().toDateString());
+  }
+
+  const checkIfDataIsExpired = (data) => {
+    if (!data || !data[0] || !data[0].endDate) return true
+
+    const firstDataObj = data[0]
+    const firstDataObjCreatedAt = firstDataObj.endDate
+
+    const hasEndDatePassed = isDateBeforeToday(firstDataObjCreatedAt)
+    return hasEndDatePassed
+  }
+
+  const getDataFromLocalStorage = () => {
+    const data = localStorage[LOCAL_STORAGE_QL_KEY]
+    const jsonParsedData = data ? JSON.parse(data) : null
+
+    const isExpired = checkIfDataIsExpired (jsonParsedData)
+    if (!isExpired) {
+      localStorage.setItem(LOCAL_STORAGE_QL_KEY, null)
+      return null
+    }
+
+    return jsonParsedData
+  }
+
+  const setDataFromToLocalStorage = (data) => {
+    localStorage.setItem(LOCAL_STORAGE_QL_KEY, data)
+  }
+
+  const updateDataFromLocalStorage = (data) => {
+    dispatch(setQuranList(data))
+  }
+
+  const updateQuranList = () => {
+    setShowRetryButton(false)
+
+    const data = getDataFromLocalStorage()
+    if (data) updateDataFromLocalStorage(data)
+    else updateDataFromServer()
   }
 
   useEffect(() => {
