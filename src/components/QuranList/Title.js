a35a78a97c3c2bf1b81ac4c3724586f61b8c7b91
Askari Iqbal <askari.iqbal14@gmail.com>
2026-02-23 23:07:07 +0000

    feat: go back and forward days to see list

--- src/components/QuranList/Title.js
+++ src/components/QuranList/Title.js
@@ -1,6 +1,52 @@
+import { useState } from "react";
 import Moment from 'react-moment';
 
-const Title = ({listNumber, title, description, startDate, endDate}) => {
+const Title = ({listNumber, title, description, startDate, endDate, onDateChange}) => {
+
+  if (!endDate) endDate = startDate
+
+  const [currentStartDate, setCurrentStartDate] = useState(new Date(startDate));
+  const [currentEndDate, setCurrentEndDate] = useState(new Date(endDate));
+
+  const cutOffStartDate = new Date("2026-02-17 05:00:00+00")
+  const cutOffEndDate = new Date("2026-03-21 05:00:00+00")
+
+
+  const handleDateChangePrev = () => {
+    const newStart = new Date(currentStartDate);
+    const newEnd = new Date(currentEndDate);
+
+    newStart.setDate(newStart.getDate() - 1);
+    newEnd.setDate(newEnd.getDate() - 1);
+
+    if (newStart < cutOffStartDate) {
+      return
+    }
+
+    setCurrentStartDate(newStart);
+    setCurrentEndDate(newEnd);
+    
+    onDateChange(1)
+  }
+
+  const handleDateChangeNext = () => {
+    const newStart = new Date(currentStartDate);
+    const newEnd = new Date(currentEndDate);
+
+    newStart.setDate(newStart.getDate() + 1);
+    newEnd.setDate(newEnd.getDate() + 1);
+
+    if (newEnd > cutOffEndDate) {
+      return
+    }
+
+    setCurrentStartDate(newStart);
+    setCurrentEndDate(newEnd);
+
+
+    onDateChange(-1)
+  }
+
   return (
     <>
       <div className="bg-sky-200 font-bold text-center text-lg md:text-3xl w-11/12 md:w-2/3 pt-5 pb-5 mb-4 ml-auto mr-auto">
@@ -8,7 +54,20 @@ const Title = ({listNumber, title, description, startDate, endDate}) => {
       </div>
 
       <div className="bg-sky-200 font-bold text-center text-lg md:text-3xl w-11/12 md:w-2/3 pt-5 pb-5 mt-4 ml-auto mr-auto">
-        <Moment format='DD MMMM'>{startDate}</Moment> - <Moment format='DD MMMM'>{endDate}</Moment>
+
+        <button
+          onClick={handleDateChangePrev}
+          className="px-4 py-2 mr-4 bg-white rounded-full shadow hover:bg-gray-200"
+        >
+          ←
+        </button>
+        <Moment format='DD MMMM'>{currentStartDate}</Moment> - <Moment format='DD MMMM'>{currentEndDate}</Moment>
+        <button
+          onClick={handleDateChangeNext}
+          className="px-4 py-2 ml-4 bg-white rounded-full shadow hover:bg-gray-200"
+        >
+          →
+        </button>
       </div>
 
       <div className="bg-sky-200 font-bold text-center text-lg md:text-3xl w-11/12 md:w-2/3 pt-5 pb-5 mt-4 ml-auto mr-auto">
--- src/components/QuranList/index.js
+++ src/components/QuranList/index.js
@@ -2,6 +2,7 @@ import GoBackButton from './GoBackButton'
 import Title from './Title'
 import Content from './Content'
 import { useSelector } from 'react-redux'
+import { useState } from 'react'
 
 const QuranList = () => {
 
@@ -11,11 +12,45 @@ const QuranList = () => {
   const listIndex = selectedList - 1
   const quranList = totalQuranList.length > listIndex ? totalQuranList[listIndex] : {}
 
+  const [quranListUsers, setQuranListUsers] = useState(quranList?.users)
+
+  const handleDateChange = (numDays) => {
+    if (numDays > 0) {
+      const rotatedList = rotateLeft(quranListUsers , numDays)
+      setQuranListUsers(rotatedList)
+    }
+
+    if (numDays < 0) {
+      const rotatedList = rotateRight(quranListUsers , -1 * numDays)
+      setQuranListUsers(rotatedList)
+    }
+  }
+
+  const rotateRight = (arr, shift) => {
+    const n = arr.length;
+    const k = shift % n; // handle shifts larger than array
+    
+    if (n === 0) return arr;
+    if (k === 0) return arr.slice();
+    
+    return arr.slice(-k).concat(arr.slice(0, n - k));
+  }
+
+  const rotateLeft = (arr, shift) => {
+    const n = arr.length;
+    const k = shift % n; // handle shifts larger than array
+    
+    if (n === 0) return arr;
+    if (k === 0) return arr.slice();
+    
+    return arr.slice(k).concat(arr.slice(0, k));
+  }
+
   return (
     <>
       <GoBackButton />
-      {quranList && <Title title={quranList.title} listNumber={selectedList} description={quranList.description} startDate={quranList.startDate} endDate={quranList.endDate} /> }
-      {quranList && <Content users={quranList.users} /> }
+      {quranList && <Title title={quranList.title} listNumber={selectedList} description={quranList.description} startDate={quranList.startDate} endDate={quranList.endDate} onDateChange={handleDateChange}/> }
+      {quranList && <Content users={quranListUsers} /> }
     </>
   )
 }
